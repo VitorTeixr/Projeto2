97613315ee87751a910fe0f8efeb1afb23c4f2dd
VitorTeixr <teixeiravitor474@gmail.com>
2024-08-01 19:59:32 -0300

    Video de fundo

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,13 @@
 extends Control
 class_name MainMenu
 
+
 func _ready() -> void:
 	for _button in get_tree().get_nodes_in_group("button"):
 		_button.pressed.connect(_on_button_pressed.bind(_button))
 		
+	if not BackgroundMusic.playing:
+		BackgroundMusic.play()
 
 func _on_button_pressed(_button: Button) -> void:
 	match _button.name:
