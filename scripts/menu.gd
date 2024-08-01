4789870c59b51efb53e91f8d8497477ef4e8678a
VitorTeixr <teixeiravitor474@gmail.com>
2024-08-01 16:33:45 -0300

    Init

---
+++ scripts/menu.gd
@@ -0,0 +1,22 @@
+extends Control
+class_name MainMenu
+
+func _ready() -> void:
+	for _button in get_tree().get_nodes_in_group("button"):
+		_button.pressed.connect(_on_button_pressed.bind(_button))
+		
+
+func _on_button_pressed(_button: Button) -> void:
+	match _button.name:
+		"NewGame":
+			get_tree().change_scene_to_file("res://level.tscn")
+			
+		"Options":
+			get_tree().change_scene_to_file("res://Options.tscn")
+			
+		"Credits":
+			pass
+			
+		"Left":
+			get_tree().quit()
+		
