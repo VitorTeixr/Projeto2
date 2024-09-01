ef940fb51eea350b922b3eb85ec419d1b404a3a8
VitorTeixr <teixeiravitor474@gmail.com>
2024-08-31 22:01:28 -0300

    dsfd

--- tela_gameplay.gd
+++ tela_gameplay.gd
@@ -16,9 +16,8 @@ func _ready() -> void:
 	for button in get_tree().get_nodes_in_group('button'):
 		button.pressed.connect(_on_button_pressed.bind(button))
 	
-	# Conecta o botão de verificação de resposta ao sinal 'pressed'
-	var button_check = get_node("Label/Button")  # Certifique-se de que este é o caminho correto para o botão
-	button_check.pressed.connect(_on_check_button_pressed)
+	# Configura o botão como não toggle
+	get_node("Label/Button").toggle_mode = false
 
 func _on_button_pressed(button: Button):
 	match button.name:
@@ -26,28 +25,26 @@ func _on_button_pressed(button: Button):
 			resposta_selecionada = 'b1'
 		'b2':
 			resposta_selecionada = 'b2'
-
-func _on_check_button_pressed():
+	
 	_verificar_resposta()
 
 func _verificar_resposta():
 	if resposta_selecionada == problemas[problema_atual]['resposta']:
-		get_tree().change_scene_to_file("res://Interface/windows95.tscn")
 		print("Acertou!")
 	else:
 		print("Resposta errada.")
 
 func _atualizar_problema():
 	if problema_atual < problemas.size():
-		var label = get_node("ScrollContainer2/VBoxContainer/Label2")
-		if label:
-			label.text = problemas[problema_atual]['problema']
-		else:
-			print("Nó do Label não encontrado.")
+		get_node("ScrollContainer2/VBoxContainer/Label2").text = problemas[problema_atual]['problema']
 	else:
 		print("Todos os problemas foram respondidos.")
 
+func _on_button_button_down():
+	# Pode não ser necessário se a lógica estiver em _on_button_pressed()
+	pass
 
-func _on_sair_pressed():
-	get_tree().change_scene_to_file("res://Interface/windows95.tscn")
-	pass # Replace with function body.
+func _on_option_button_item_selected(index):
+	# Atualiza o índice do problema se você estiver usando um OptionButton
+	problema_atual = index
+	_atualizar_problema()
